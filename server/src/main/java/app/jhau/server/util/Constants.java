8b6ae19a0a73632533426c10989459c2beeb6ad9
Jhau <1085688359@qq.com>
2022-12-14 13:55:37 +0800

    Some changes.

--- server/src/main/java/app/jhau/server/util/Constants.java
+++ server/src/main/java/app/jhau/server/util/Constants.java
@@ -4,6 +4,5 @@ import app.jhau.server.BuildConfig;
 
 public class Constants {
     public static final String APPLICATION_ID = BuildConfig.APPLICATION_ID;
-    public static final String APP_PROCESS_NAME = APPLICATION_ID + ".server";
     public static final String SERVER_BINDER_KEY = "binder";
 }
